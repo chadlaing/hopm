58d1584b2400036d793f7adc44ed2a4a190ce4a9
Chad Laing <chadlaing@gmail.com>
2015-11-25 12:01:14 -0700

    read dir and get file contents

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings  #-}
+
 module Main where
 
-import Lib
+import Prelude ()
+import System.IO (print, IO)
+import Control.Monad (mapM)
+import System.Directory (getDirectoryContents)
+import Data.Functor (fmap)
+import ParseKineticFile
+import qualified Data.ByteString.Lazy.Char8 as BS
+
 
 main :: IO ()
-main = someFunc
+main = do
+    let theDirectory = "/home/chad/workspace/ecoli_omnilog/data/kinetic/AllEdited"
+    allFiles <- getDirectoryContents theDirectory
+    allData <- mapM BS.readFile allFiles
+    let (a:b) = allData
+    print a
+
+
+
+
+
