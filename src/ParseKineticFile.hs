bb9757e0900358ef06f665caeafb666ffb98b066
Chad Laing <chadlaing@gmail.com>
2015-12-01 15:55:37 -0700

    Splitting Well and Plate description to separate file to lessen compilation time.

--- src/ParseKineticFile.hs
+++ src/ParseKineticFile.hs
@@ -41,31 +41,8 @@ import Control.Applicative
 import Data.Bool (otherwise)
 import Data.Hashable
 import GHC.Generics               (Generic)
+import PlateWell
 
--- | All 20 possible plates defined for the omnilog system.
--- Create experiment is used to assign the correct description.
--- Both Plates and Wells are only to be created using createExperiment
-data Plate = PM1{description :: T.Text}
-           | PM2{description :: T.Text}
-           | PM3{description :: T.Text}
-           | PM4{description :: T.Text}
-           | PM5{description :: T.Text}
-           | PM6{description :: T.Text}
-           | PM7{description :: T.Text}
-           | PM8{description :: T.Text}
-           | PM9{description :: T.Text}
-           | PM10{description :: T.Text}
-           | PM11{description :: T.Text}
-           | PM12{description :: T.Text}
-           | PM13{description :: T.Text}
-           | PM14{description :: T.Text}
-           | PM15{description :: T.Text}
-           | PM16{description :: T.Text}
-           | PM17{description :: T.Text}
-           | PM18{description :: T.Text}
-           | PM19{description :: T.Text}
-           | PM20{description :: T.Text}
-           deriving(Ord, Eq, Show, Read)
 -- | Define all possible 96 wells for the omnilog plates
 -- createExperiment is used to define the proper annotations.
 -- In order for type Well to be used as a hash key, it needs to be
